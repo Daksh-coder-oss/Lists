7497767fa64e5fa84827910a3b0cb08c47a490d4
Daksh-coder-oss <vv0039190@gmail.com>
2024-12-29 11:13:30 +0000

    Add files via upload

---
+++ List.py
@@ -0,0 +1,22 @@
+x=[]
+print(x)
+
+y=[9,8,"t"]
+
+print(y)
+
+z=[9,6,7]
+g=5
+a=[]
+for i in range(len(z)):
+    x=z[i]*g
+    a.append(x)
+print(a)
+
+c=z[::-1]
+print(c)
+fruits=["apple","banana","orange"]
+fruits.reverse()
+print(fruits)
+z.reverse()
+print(z)
